184ab782faff05a73abbb9304f57b766a0a0359f
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-01-04 22:28:08 +0500

    add handle bars

--- src/app.js
+++ src/app.js
@@ -2,15 +2,24 @@ const express= require('express');
 const path=require('path')
 const app=express();
 const port =process.env.PORT || 3000;
+const hbs=require('hbs');
 
+const templatePath=path.join(__dirname,'../template/views')
+const partialsPath=path.join(__dirname,'../template/partials');
 const static_path=path.join(__dirname,'../public')
 app.use(express.static(static_path))
+//  to set view engine
+app.set('view engine','hbs');
+hbs.registerPartials(partialsPath);
+app.set('views',templatePath);
 
 app.get('/',(req,res)=>{
-    res.send("hello i am from index file is runing page");
+    res.render('index');
 
 })
-
+app.get('*',(req,res)=>{
+    res.send("Opps Page dose not exists")
+})
 app.listen(port,()=>{
        console.log(`listening from the port no ${port} ......`)
 })
